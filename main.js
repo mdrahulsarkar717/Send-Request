b1c959791a1c3f7481bb3d3afeb9b79d54bba954
mdrahulsarkar717 <mdrahulsarkar717@gmail.com>
2024-07-09 09:17:32 +0600

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector('#btn');
+let thanks = document.querySelector('.thanks');
+let form = document.querySelector('#form');
+let email = document.querySelector('#email');
+
+
+
+
+
+function thankyou() {
+     form.classList.add("pl");
+     thanks.classList.remove("pl");
+}
